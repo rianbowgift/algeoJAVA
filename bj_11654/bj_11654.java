be4dda12aca8e01245abff042ff712997b2b34ed
rianbowgift <48279678+rianbowgift@users.noreply.github.com>
2019-06-27 18:26:18 +0900

    Add files via upload

---
+++ bj_11654/bj_11654.java
@@ -0,0 +1,16 @@
+package bj_11654;
+
+import java.util.Scanner;
+
+public class bj_11654 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		
+		int num = sc.next().charAt(0);
+		System.out.print(num);
+	
+	}
+}
